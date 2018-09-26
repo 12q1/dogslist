84e94f232eb972fe90a86182a526d766d711829b
12q1 <thomasham89@gmail.com>
2018-09-26 16:58:55 +0200

    semi-working, test on home serv

--- src/App.js
+++ src/App.js
@@ -1,18 +1,20 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import DogsListContainer from './components/DogsListContainer.js'
+import { Route } from 'react-router-dom'
+import DogBreedImages from './components/DogBreedImages'
+import DogBreedImagesContainer from './components/DogBreedImagesContainer'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <main>
+         <Route exact path="/" component={DogsListContainer} />
+         <Route path="/dog-breeds/:breed" component={DogBreedImages} />
+         <Route exact path="/" component={DogBreedImagesContainer} />
+        </main>
       </div>
     );
   }
